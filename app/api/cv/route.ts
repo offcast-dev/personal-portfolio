c4d1b9daa73657549fa4a22ff4bda311829c6e56
Seva Deriushkin <seva.deriushkin@gmail.com>
2026-02-10 17:23:12 +0100

    updates CV

--- app/api/cv/route.ts
+++ app/api/cv/route.ts
@@ -3,10 +3,10 @@ import path from "path";
 import fs from "fs";
 
 export async function GET() {
-  const filePath = path.resolve(".", "public/seva-deriushkin-cv.pdf");
+  const filePath = path.resolve(".", "public/Vsevolod_Deriushkin_CV.pdf");
   const fileBuffer = fs.readFileSync(filePath);
 
-  return new NextResponse(fileBuffer, {
+  return new NextResponse(new Uint8Array(fileBuffer), {
     headers: { "content-type": "application/pdf" },
   });
 }
